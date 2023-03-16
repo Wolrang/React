0c37ae95233ff324a6a1a0bb5703b5ccdb799b25
Wolrang <dkfud252@naver.com>
2023-03-16 18:39:47 +0900

    🎉 React

--- src/component/Blog/Blog.jsx
+++ src/component/Blog/Blog.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './Blog.css'
-import {useState} from 'react';
+import { useState } from 'react';
 
 const Blog = () => {
   const [title, setTitle] = useState([
@@ -8,7 +8,7 @@ const Blog = () => {
     "Title2",
     "Title3",
   ])
-  
+
   const name = ["이름1", "이름2", "이름3"]
 
   const [blogList, setBlogList] = useState(title);
@@ -22,7 +22,7 @@ const Blog = () => {
   }
 
   // 모달창을 띄우기 위한 상태값 / 초기값 = false
-  let [modal,setModal] = useState(false);
+  let [modal, setModal] = useState(false);
   // 모달창에 표시될 제목 글의 인덱스 / 초기값 = 0
   let [modalTitle, setModalTitle] = useState(0);
 
@@ -33,11 +33,11 @@ const Blog = () => {
       </div>
 
       {
-        title.map((item, i)=>{
+        title.map((item, i) => {
           return (
             <div key={i} className='list'>
               {/* map으로 html요소를 반복할 때는 고유의 key값이 필요함 */}
-              <div style={{cursor: 'pointer'}} onClick={()=>
+              <div style={{ cursor: 'pointer' }} onClick={() =>
                 // {
                 // 조건문
                 //   if(modal && modalTitle === i) {
@@ -47,13 +47,17 @@ const Blog = () => {
                 //     setModalTitle(i)
                 //   }
                 // }
+
                 // 삼항연산자에 onClick(()=>{modal && modalTitle === i~~ }) 작성시 에러 발생
                 // Expected an assignment or function call and instead saw an expression
                 // 할당 또는 함수를 예상 했는데 표현식이 나왔다는 에러
+
                 // 해결방법1 : { } 중괄호 제거
                 // onClick(()=> modal && modalTitle === i~~ )
+
                 // 해결방법2 : 조건 앞에 retrun 추가
                 // onClick(()=>{return modal && modalTitle === i~~ })
+
                 // 에러 발생 이유
                 // 1. {} 중괄호를 생략해야 함수의 body가 표현식으로 인식하기 때문에 return이 없어도 값을 반환한다.
                 // 2. 함수 내부에서 삼항연산자를 사용하여 값을 반환하려면 반드시 return이 있어야 한다. r
@@ -63,19 +67,19 @@ const Blog = () => {
                 <h4>{item}</h4>
                 <p>안녕하세요 저는 {name[i]} 입니다.</p>
               </div>
-                <button onClick={() => {deleteList(i)}}>글삭제</button>
+              <button onClick={() => { deleteList(i) }}>글삭제</button>
             </div>
           )
         })
       }
-      
+
       {/*  */}
-      {modal && <Modal modalTitle={modalTitle} title={title} name={name} closeModal={()=> setModal(false)}/>}
+      {modal && <Modal modalTitle={modalTitle} title={title} name={name} closeModal={() => setModal(false)} />}
     </div>
   )
 }
 
-function Modal(props){
+function Modal(props) {
   return (
     <div className='modal'>
       {/*  */}
