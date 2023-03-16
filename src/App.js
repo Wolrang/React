c94dafa9a83c6c468d2db22ffa710b61b871579c
Wolrang <dkfud252@naver.com>
2023-03-16 17:19:36 +0900

    🎉 React

--- src/App.js
+++ src/App.js
@@ -8,18 +8,24 @@ import Blog from './component/Blog/Blog';
 import { Routes, Route } from 'react-router-dom';
 import Sub1 from './component/Sub1';
 import Menu2 from './component/Menu2';
+import { Button } from 'react-bootstrap';
+import Nav from './component/Nav'
+import Card from './component/Card'
 
 function App() {
   return (
     <div className="App">
-      <Header></Header>
+      <Nav></Nav>
+      <Card></Card>
+      {/* <Button variant="primary">Primary</Button>{' '} */}
+      {/* <Header></Header> */}
 
-      <Routes>
+      {/* <Routes>
         <Route path='/' element={<Main></Main>}></Route>
         <Route path='/menu1' element={<h1>menu1 페이지</h1>}></Route>
         <Route path='/menu2' element={<Menu2></Menu2>}></Route>
         <Route path='/menu3' element={<h1>menu3 페이지</h1>}></Route>
-      </Routes>
+      </Routes> */}
       {/* <Blog></Blog> */}
     </div>
   );
